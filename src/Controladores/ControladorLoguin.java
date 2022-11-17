a6e391a7f3d256a92c1da38f2d91371694282105
tasita <thomasmauricio521@gmail.com>
2022-11-17 11:13:28 -0500

    first commit

---
+++ src/Controladores/ControladorLoguin.java
@@ -0,0 +1,17 @@
+package Controladores;
+
+import Vistas.Vistalogin;
+
+public class ControladorLoguin {
+    
+    Vistalogin vistalogin = new Vistalogin();
+    
+    public void inicio() {
+        vistalogin.setVisible(true);
+    }
+    
+    public void btn_entrar() {
+        //entrar
+    }
+    
+}
---
+++ src/educativofesc/EducativoFesC.java
@@ -0,0 +1,14 @@
+package educativofesc;
+
+import Controladores.ControladorLoguin;
+
+public class EducativoFesC {
+
+    public static void main(String[] args) {
+        ControladorLoguin loguin = new ControladorLoguin();
+
+        loguin.inicio();
+
+    }
+
+}
